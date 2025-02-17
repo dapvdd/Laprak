31bd7325fd9d26434077392c8e3d126074a03a51
dapvdd <116717934+dapvdd@users.noreply.github.com>
2025-02-17 14:02:57 +0700

    Add files via upload

---
+++ no2.py
@@ -0,0 +1,19 @@
+class Kucing:
+    warna : None
+    umur : None
+    
+kucing1 = Kucing()
+kucing1.warna = 'hitam'
+kucing1.umur = '1 bulan'
+
+kucing2 = Kucing()
+kucing2.warna = 'putih'
+kucing2.umur = '2 bulan'
+
+kucing3 = Kucing()
+kucing3.warna = 'merah'
+kucing3.umur = '3 bulan'
+
+print('kucing', kucing1.warna, kucing1.umur)
+print('kucing', kucing2.warna, kucing2.umur)
+print('kucing', kucing3.warna, kucing3.umur)
